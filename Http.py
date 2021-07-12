272334c9f8430e980571b4c780535098696b4ab7
Jackson <wolfyun2015@126.com>
2021-07-12 09:12:10 +0800

    first commit

---
+++ Http.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3
+# -*- coding:utf-8 -*-
+__author__="Jackson Law"
+__version__="1.0.00"
+# ----------------------------------------------------------------------------------------------------------
+def main():
+    print("hello,world!")
+
+if __name__ == "__main__":
+    main()
+else:
+    print("CANNOT BE RUN SEPARATELY!!!")
